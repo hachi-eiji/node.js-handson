1fc7e797cb1eed28c73788ad4a602d245f92abeb
Eiji Hachiya <hachiyae@gmail.com>
2012-11-05 17:35:59 +0700

    websocketを追加
    まだ動かない

--- websocket/app.js
+++ websocket/app.js
@@ -1,4 +1,8 @@
-var TwBot = require('twbot').TwBot;
+var TwBot = require('twbot').TwBot,
+    http  = require('http'),
+    fs = require('fs'),
+    util = require('util'),
+    io = require('socket.io');
 
 var TW_OPTION = {
   consumerKey:'',
@@ -8,9 +12,43 @@ var TW_OPTION = {
 };
 
 var bot = new TwBot(TW_OPTION);
-
-//lib/bot.loadPlugin('twbot/lib/plugins/debug');
-bot.on('status', function(tweet){
-  console.log('%s : %s', tweet.user.screen_name, tweet.text);
-});
 bot.startUserStream();
+
+var server = http.createServer(function(req, res){
+  res.writeHeader(200, {'Content-Type': 'text/html'});
+  var read = fs.createReadStream(__dirname + '/websocket.html');
+  read.on('error', function(err){
+    res.end(err.stack);
+  });
+
+  read.on('data', function(data){
+    res.write(data);
+  });
+  read.on('end', function(){
+    res.end();
+  });
+});
+server.listen(3000);
+
+var socket = io.listen(server);
+console.log(socket);
+socket.on('connection', function(client){
+  util.log('call connection');
+
+  var fun = function(tweet){
+    //console.log('%s : %s', tweet.user.screen_name, tweet.text);
+    util.log('call status');
+    client.send(JSON.stringify(tweet));
+  };
+  bot.on('status', fun);
+
+  client.on('message', function(message){
+    util.log('receive message');
+  });
+
+  client.on('disconnect', function(){
+    util.log('call disconnect');
+    // クライアントが消えたらイベントを外す
+    bot.removeListener('status', fun);
+  });
+});
